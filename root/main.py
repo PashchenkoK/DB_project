4aa37ad18d381bebb4af71e0f2e97d01941382cf
Katia Pashchenko <ketrinpashchenko@ukr.net>
2020-05-07 20:49:21 +0300

    lab2

--- root/main.py
+++ root/main.py
@@ -1,10 +1,25 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import json
 app = Flask(__name__)
+with open("editions.json", "r") as edition:
+	editions = json.load(edition)
 
-@app.route('/')
-def main():
-	return 'Hello world!'
+with open("customers.json", "r") as customer:
+	customers = json.load(customer)
+
+
+@app.route('/api/<action>', methods = ['GET', 'POST'])
+def apiget(action):
+	if action == 'edition':
+		return render_template('edition.html', edition = editions)
+	elif action == 'customer': #замовник
+		return render_template('customer.html', customer = customers)
+	elif action == 'all':
+		return render_template('all.html', edition = editions, customer = customers)
+	else:
+		return render_template('error.html')
+
 
 if __name__ == '__main__':
+
 	app.run(debug = True)
