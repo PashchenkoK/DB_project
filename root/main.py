33c7bb4d455ab46e31d16ccceeb2db226f41fdf0
Katia Pashchenko <ketrinpashchenko@ukr.net>
2020-05-09 13:52:36 +0300

    '675'

--- root/main.py
+++ root/main.py
@@ -3,7 +3,7 @@ import json
 
 app = Flask(__name__)
 
-"""with open('customers.json', "r") as customer:
+with open('customers.json', "r") as customer:
 	customers = json.load(customer)
 
 
@@ -20,11 +20,8 @@ def apiget(action):
 	elif action == 'all':
 		return render_template('all.html', edition = editions, customer = customers)
 	else:
-		return render_template('error.html')"""
+		return render_template('error.html')
 
-@app.route('/api', methods = ['GET', 'POST'])
-def apiget():
-	return render_template('error.html') 
 
 if __name__ == '__main__':
 	app.run(debug = True)
